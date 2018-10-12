afe954d535f0bb0ed2966f5d1f0799c67063febd
RodrigoUFC <31975783+RodrigoUFC@users.noreply.github.com>
2018-10-12 17:18:55 -0300

    Versao_Final

--- Sistemas Distribuidos/Servidor_Filmes/TCP_client.py
+++ Sistemas Distribuidos/Servidor_Filmes/TCP_client.py
@@ -10,7 +10,9 @@ print('Bem vindo! Para obter a lista de filmes, digite "Filme" no console. Para 
 msg = input()
 while msg != 's':
     tcpServer.send(msg.encode('utf-8'))
-    respostaJSON = tcpServer.recv(1024).decode('utf-8')
-    print(respostaJSON)
+    resposta = tcpServer.recv(4096).decode('utf-8')
+    respostaJSON = json.loads(resposta)
+    for i in range(len(respostaJSON)):
+        print("Filme "+str(i)+"\nID: "+respostaJSON[i]['FilmeID']+"\nTítulo: "+respostaJSON[i]['Titulo']+"\nAno: "+respostaJSON[i]['Ano']+"\nAvaliação IMDB: "+respostaJSON[i]['Avaliacao_IMDB']+"\nTrês filmes mais relacionados: "+respostaJSON[i]['Tres_filmes_mais_relacionados'][0]+", "+respostaJSON[i]['Tres_filmes_mais_relacionados'][1]+", "+respostaJSON[i]['Tres_filmes_mais_relacionados'][2]+"\nLink para trailer no Youtube: "+respostaJSON[i]['Link_para_trailer_Youtube'])
     msg = input()
 tcpServer.close()
--- Sistemas Distribuidos/Servidor_Filmes/TCP_server.py
+++ Sistemas Distribuidos/Servidor_Filmes/TCP_server.py
@@ -5,41 +5,41 @@ filmes = []
 filmes.append({
     "FilmeID": "tt2674426",
     "Titulo": "Como eu era antes de voce",
-    "Ano": 2016,
-    "Avaliacao_IMDB": 7.4,
+    "Ano": "2016",
+    "Avaliacao_IMDB": "7.4",
     "Tres_filmes_mais_relacionados": ["A culpa e das estrelas","Diario de uma paixao","Simplesmente acontece"],
     "Link_para_trailer_Youtube": "https://www.youtube.com/watch?v=PnqUs3xiAVI"
 })
 filmes.append({
   "FilmeID": "0001",
   "Titulo": "MyLife",
-  "Ano": 2016,
-  "Avaliacao_IMDB": 3,
-  "Tres_filmes_mais_relacionados": ["MyLife1", "MyLife2", "MyLife3" ],
+  "Ano": "2016",
+  "Avaliacao_IMDB": "3",
+  "Tres_filmes_mais_relacionados": ["MyLife1", "MyLife2", "MyLife3"],
   "Link_para_trailer_Youtube": "www.youtube/0001.com"
 })
 filmes.append({
   "FilmeID": "0002",
   "Titulo": "MyLife2",
-  "Ano": 2016,
-  "Avaliacao_IMDB": 4,
-  "Tres_filmes_mais_relacionados": ["MyLife1", "MyLife2", "MyLife3" ],
+  "Ano": "2016",
+  "Avaliacao_IMDB": "4",
+  "Tres_filmes_mais_relacionados": ["MyLife1", "MyLife2", "MyLife3"],
   "Link_para_trailer_Youtube": "www.youtube/0002.com"
 })
 filmes.append({
   "FilmeID": "0003",
   "Titulo": "MyLife2",
-  "Ano": 2003,
-  "Avaliacao_IMDB": 5,
-  "Tres_filmes_mais_relacionados": ["MyLife1", "MyLife2", "MyLife3" ],
+  "Ano": "2003",
+  "Avaliacao_IMDB": "5",
+  "Tres_filmes_mais_relacionados": ["MyLife1", "MyLife2", "MyLife3"],
   "Link_para_trailer_Youtube": "www.youtube/0003.com"
 })
 filmes.append({
   "FilmeID": "0004",
   "Titulo": "MyLife2",
-  "Ano": 2004,
-  "Avaliacao_IMDB": 6,
-  "Tres_filmes_mais_relacionados": ["MyLife1", "MyLife2", "MyLife3" ],
+  "Ano": "2004",
+  "Avaliacao_IMDB": "6",
+  "Tres_filmes_mais_relacionados": ["MyLife1", "MyLife2", "MyLife3"],
   "Link_para_trailer_Youtube": "www.youtube/0004.com"
 })
 filmesJSON = json.dumps(filmes)
@@ -53,9 +53,9 @@ while True:
     conexao, cliente = tcpServer.accept()
     print('Conectado com', cliente)
     while True:
-        msg = conexao.recv(1024).decode('utf-8')
+        msg = conexao.recv(4096).decode('utf-8')
         if msg == 'Filme':
-            conexao.send(filmesJSON.encode('utf-8'))
+            conexao.sendall(filmesJSON.encode('utf-8'))
             print(cliente, msg)
         if msg == 's':
             retorno = 'Desconectando do servidor.'
